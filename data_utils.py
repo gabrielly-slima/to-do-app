fadc54c5d70ed3ac75e96967106a568c5632599a
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-03 12:09:53 -0300

    starting the main code

---
+++ data_utils.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+current_datetime = datetime.now()
+
+def convert_date_to_pt ():
+    week_pt = {"Sunday": "Domingo", 
+               "Monday":"Segunda-feira", 
+                "Tuesday":"Terça-feira",
+                "Wednesday":"Quarta-feira",
+                "Thursday":"Quinta-feira",
+                "Friday":"Sexta-feira",
+                "Saturday":"Sábado"}
+
+    months_pt = {"January":"janeiro", "February":"fevereiro",
+                "March":"maio", "April":"abril", "May":"maio",
+                "June":"junho", "July":"julho","August":"agosto",
+                "September":"setembro","October":"outubro",
+                "November":"novembro","December":"dezembro"} 
+
+    week_day = current_datetime.strftime("%A")
+    day = current_datetime.strftime("%d")
+    month = current_datetime.strftime("%B")
+    year = current_datetime.strftime("%Y")  
+
+    # Converting days of the week from english to portuguese 
+    acess_keys_week = list(week_pt.keys())
+    for i in acess_keys_week:
+        if i == week_day:
+            week_day = week_pt[i]
+    
+    acess_keys_month = list(months_pt.keys())
+    for i in acess_keys_month:
+        if i == month:
+            month = months_pt[i]
+    
+    formatted_date = (f"Hoje é {week_day}, dia {day} de {month} de {year}")
+    return formatted_date
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from datetime import datetime
+from data_utils import convert_date_to_pt
 
 janela = tk.Tk()
 janela.title("To do list")
 
-semana = []
-rotulo = Label(janela, text= "Hoje é dia ")
+rotulo = tk.Label(janela, text= convert_date_to_pt())
+rotulo.pack()
 
 janela.mainloop()
